eaac6bce93d0df8665d39d38befc1b9b7efac161
Lude71 <ludekigen767@gmail.com>
2025-07-01 12:06:39 +0300

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,32 @@
+# name=input('what is your name? ')
+# colour=input('what is your favourite colour? ')
+# print(name + ' likes ' + colour)
+
+school = ('Dedan kimathi universit')
+print(school[3:7])
+print(f"{school}")
+print(school)
+name=('eliud kigen')
+print(name.upper())
+print('Hello world')
+print('my name is eliud kigen \n i am from kimathi')
+print(""""this is the strangest 
+      way to print code in multiple lines i know""")
+print("my name is eliud\" i am in second year")
+print('waht about\\news')
+print('There was once a movie staricon')
+print('who prefered to sleepwhit the light on.')
+print('a device that sure glowed')
+print('and lit up the night using python!')
+poem = """Roses are red,
+Violets are blue,
+python is fun,
+And powerful too."""
+print (poem)
+poem = "Roses are red,\nviolets are blue,\npython is fun,\nAnd powerful too"
+print(poem)
+game = input('mancity is playoing everton')
+school=input('DEDAN KIMATHI')
+print(school)
+name=input('what is your name? ')
+print(name)
